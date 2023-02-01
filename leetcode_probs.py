09a5928be857cfd348ea23e84ea1f845b662b7d5
Olivia Smith <olivia.f.smith@gmail.com>
2023-01-31 20:16:21 -0800

    +removeDuplicates

--- leetcode_probs.py
+++ leetcode_probs.py
@@ -24,11 +24,28 @@ class Solution:
         # Given an integer x, return true if x is a palindrome, and false otherwise.
         return x >= 0 and x == int(str(x)[::-1])
 
+    def removeDuplicates(self, nums: List[int]) -> int:
+        #Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once
+        #Return k after placing the final result in the first k slots of nums.
+        #It is guaranteed that the given array is a sorted array.
+        # Ex. [1, 1, 1, 2, 2, 3, 3 ] -> [1, 2, 3]
 
-s = Solution()
-# s.reverse(x=123) # 321
-# s.reverse(x=-123) # -321
-# s.reverse(x=120) # 21
+        # First Index is responsible for writing unique values in our input array, while Second Index will read the input array and pass all the distinct elements to First Index. We continue the above steps until the second index reaches the end of an array
+        #Check if prev elem is different from current elem.
+        # If found different, perform arr[j] = arr[i] and increment j += 1
+        # Increment i+=1 until end of array is reached.
+
+        size = len(nums)
+        j = 1
+        for i in range(1, size):
+            # Find unique element
+            if nums[i - 1] != nums[i]:
+                #updating j in our main array
+                nums[j] = nums[i]
+                # Incrementing j (insert) count by 1
+                j += 1
+        return j
+
 
 
 # Def of singly - linked list (https://leetcode.com/problems/palindrome-linked-list/description/)
@@ -36,11 +53,8 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-    
-    def printself(self):
-        print(self.val)
 
-class Solution:
+class SolutionNode:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         arr = [head.val]
         while head.next:
